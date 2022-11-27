1bcc9ec9499cf0c742c162d119f69c0bfce16b18
Al Noman Mahib <alnomanmatubber2001@gmail.com>
2022-11-27 13:09:06 +0600

    all buyer and all seller loaded

--- src/pages/Dashboard/AllSeller/AllSeller.js
+++ src/pages/Dashboard/AllSeller/AllSeller.js
@@ -1,9 +1,38 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 const AllSeller = () => {
+  const { data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/users");
+      const data = await res.json();
+      return data;
+    },
+  });
   return (
     <div>
-      <h3>from all seller</h3>
+      <h2 className="text-2xl font-bold">All Seller</h2>
+      <div className="overflow-x-auto mt-12">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, i) => (
+              <tr key={user._id}>
+                <th>{i + 1}</th>
+                <td>{user?.name}</td>
+                <td>{user?.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
