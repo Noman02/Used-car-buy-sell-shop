aac2f46366cae8ca37868c80943a4be0d07b9ea4
Al Noman Mahib <alnomanmatubber2001@gmail.com>
2022-11-28 10:29:27 +0600

    delete functionality completed

--- src/pages/Dashboard/AllSeller/AllSeller.js
+++ src/pages/Dashboard/AllSeller/AllSeller.js
@@ -1,7 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
 
 const AllSeller = () => {
+  const [Users, setUsers] = useState([]);
   const { data: users = [] } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
@@ -10,6 +12,25 @@ const AllSeller = () => {
       return data;
     },
   });
+
+  const handleDeleteSeller = (id) => {
+    const agree = window.confirm("do you want to delete it?");
+    if (agree) {
+      fetch(`http://localhost:5000/users/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.acknowledged > 0) {
+            toast.success("User deleted successfully");
+            const remainingUsers = Users.filter((user) => user._id !== id);
+            setUsers(remainingUsers);
+            console.log(data);
+          }
+        });
+    }
+  };
+
   return (
     <div>
       <h2 className="text-2xl font-bold">All Seller</h2>
@@ -36,7 +57,12 @@ const AllSeller = () => {
                   </button>
                 </td>
                 <td>
-                  <button className="btn btn-sm btn-info">Delete</button>
+                  <button
+                    onClick={() => handleDeleteSeller(user?._id)}
+                    className="btn btn-sm btn-info"
+                  >
+                    Delete
+                  </button>
                 </td>
               </tr>
             ))}
